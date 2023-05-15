636ddb98719cba506e8624f13a6a014274bad610
jwwalls <jwwalls@users.noreply.github.com>
2023-05-15 16:00:29 -0500

    server running

--- server/app.js
+++ server/app.js
@@ -9,13 +9,13 @@ const morgan = require('morgan');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client', 'dist')));
+app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 app.use('/api', require('./api'));
 
 app.use((req, res, next) => {
   try {
-    res.status(404).send("Sorry, can't find that! :/");
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   } catch (error) {
     console.errror(error);
     throw error;
