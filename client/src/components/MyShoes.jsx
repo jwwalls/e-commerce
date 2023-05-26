a0887430454c6880453f8b3160e0cf2904e2b0ff
Mind-Rot <cricks312@gmail.com>
2023-05-26 00:36:05 -0600

    MyShoes Update

--- client/src/components/MyShoes.jsx
+++ client/src/components/MyShoes.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-function MyShoes() {
+const MyShoes = ({ token }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`${url}?userId=${token}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, [token]);
+
   return (
     <div>
-        
+      {products.map((product) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <p>{product.description}</p>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default MyShoes
+export default MyShoes;
